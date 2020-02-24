4eb5bc7ea7324c7d02270ca1ebd8568226aff7db
joaoresolvvi <joao@resolvvi.com>
2020-02-24 08:31:25 -0300

    :ok_hand: Add new input component

---
+++ packages/input/src/index.spec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+
+import Input from '.';
+
+describe('Input', () => {
+    test('renders correctly', () => {
+        const tree = renderer.create(<Input />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
