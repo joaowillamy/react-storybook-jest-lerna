679154a917aee085d458cbb6236cc51bb250a373
Joao Willamy <joao@resolvvi.com>
2020-08-04 21:01:01 -0300

    🐛 Fix export card

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -2,4 +2,5 @@ export { default as Button } from './Button';
 export { default as Header } from './Header';
 export { default as Menu } from './Menu';
 export { default as Footer } from './Footer';
+export { default as Card } from './Card';
 export { Icon } from './Icons';
