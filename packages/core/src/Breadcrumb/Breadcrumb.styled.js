c54d7eefb6f880a32c30b9d75d44982226a50b6f
Joao Willamy <joao@resolvvi.com>
2020-08-05 19:57:39 -0300

    ✨ Add breadcrumb #10

---
+++ packages/core/src/Breadcrumb/Breadcrumb.styled.js
@@ -0,0 +1,21 @@
+import styled, { css } from 'styled-components';
+
+import { chooseScrem } from '../help/chooseScreem';
+
+export const Breadcrumb = styled.div`
+  margin: 10px 0;
+`;
+
+export const BreadcrumbText = styled.p`
+  font-weight: 600;
+  font-size: 14px;
+  ${chooseScrem}
+`;
+
+export const BreadcrumbTextBlue = styled.span`
+  color: ${({ theme }) => theme.colors.bluePrimary};
+
+  svg {
+    width: ${({ theme }) => theme.iconSizes.lg};
+  }
+`;
