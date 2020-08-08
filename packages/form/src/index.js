d42eeae192103f3299c0223b760f63c512d7aa64
Joao Willamy <joao@resolvvi.com>
2020-08-07 23:30:22 -0300

    🚀 Export form components

--- packages/form/src/index.js
+++ packages/form/src/index.js
@@ -1,3 +1,5 @@
-export { default as Input } from './Input';
 export { default as Checkbox } from './Checkbox';
+export { default as Input } from './Input';
+export { default as Range } from './Range';
+export { default as Rate } from './Rate';
 export { default as Select } from './Select';
